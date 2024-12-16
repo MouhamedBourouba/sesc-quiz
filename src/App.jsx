5e9a9c9c008483cccccdf7db194b9139e03ab4c5
MouhamedBourouba <mouhamedmobiledev@gmail.com>
2024-12-16 17:16:52 +0100

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import React from "react";
+import QuizCard from './components/QuizCard/QuizCard';
+import { q1, q2 } from "./data"
 
 function App() {
-
   return (
     <>
-      <h1> QUIZ </h1>
+      <div className='flex flex-col h-screen w-screen items-center'>
+      <QuizCard totalQuestions={q1.length} currentIndex={0} options={q1[0].options} onAnswer={(index) => console.log(index)} question={q1.question}/>
+      </div>
     </>
   )
 }
